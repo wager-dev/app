de7dd296a6e3b5b7d5ca150e9f082b5a8dc353ef
DHRUV PATEL <dhruv@DHRUVs-MacBook-Pro.local>
2021-03-30 12:09:49 -0400

    Very Basic Setup

--- types.tsx
+++ types.tsx
@@ -4,14 +4,19 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Me: undefined;
+  Home: undefined;
+  Results: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type ProfileParams = {
+  Profile: undefined;
 };
 
-export type TabTwoParamList = {
-  TabTwoScreen: undefined;
+export type HomeParams = {
+  Home: undefined;
+};
+
+export type ResultsParams = {
+  Results: undefined;
 };
