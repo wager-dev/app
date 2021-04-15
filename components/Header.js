821e73b499b79aaf54517c27531cc539823ce65e
DHRUV PATEL <dhruv@DHRUVs-MacBook-Pro.local>
2021-04-15 11:59:52 -0400

    added styles and cleaned up components

--- components/Header.js
+++ components/Header.js
@@ -1,32 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, StyleSheet } from "react-native";
+import { Balance } from "./wagerbase";
 
 const ViewBoxesWithColorAndText = () => {
-    const teams = ["PHI"];
+  const teams = ["PHI"];
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        height: 100,
-        paddingLeft: 10,
-        paddingTop: 40,
-        backgroundColor: "#241f1e"
-      }}
-    >
-        <View style={{ flex: 0.3 }}>
-            <Text 
-                style={{
-                    textAlign: "center",
-                    margin: "auto",
-                    color: "white",
-                    fontSize: 40
-                }}>
-                Wager
-            </Text>
-        </View>
+    <View style={styles.container}>
+      <View style={{ flex: 0.3 }}>
+        <Text style={styles.headerText}>Wager</Text>
+      </View>
+      <View style={styles.balanceView}>
+        <Balance tokenAmount={80000} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: 100,
+    paddingLeft: 10,
+    paddingTop: 40,
+    backgroundColor: "#241f1e",
+  },
+  headerText: {
+    textAlign: "center",
+    margin: "auto",
+    color: "white",
+    fontSize: 40,
+  },
+  balanceView: {
+    flex: 0.7,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+});
 
 export default ViewBoxesWithColorAndText;
--- components/Home.js
+++ components/Home.js
@@ -1,53 +1,48 @@
 import * as React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, StyleSheet } from "react-native";
 import Header from "./Header";
 import { Icon } from "./wagerbase";
 import { Spread } from "./wagerbase";
-import { Balance } from "./wagerbase";
 
 function Home({ navigation }) {
   return (
-    <View
-      style={{
-        flexDirection: "column",
-        backgroundColor: "#606060",
-        height: "100%",
-      }}
-    >
+    <View style={styles.homeContainer}>
       <View>
         <Header />
       </View>
-      <Balance tokenAmount={80000} />
       <View>
         <View style={{ margin: "0.5em" }}>
-          <Text
-            style={{
-              fontSize: "2em",
-              color: "white",
-              textAlign: "center",
-              fontWeight: 600,
-            }}
-          >
-            Create New Wager
-          </Text>
+          <Text style={[styles.createWagerText]}>Create New Wager</Text>
         </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            height: 50,
-            margin: "2em",
-            marginTop: 0,
-            marginBottom: "1em",
-            justifyContent: "space-between",
-          }}
-        >
-          <Icon type="NBA"></Icon>
+        <View style={styles.body}>
+          <Icon type="NBA" />
           <Spread value={"+5.5"} />
         </View>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homeContainer: {
+    flexDirection: "column",
+    backgroundColor: "#606060",
+    height: "100%",
+  },
+  createWagerText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  body: {
+    display: "flex",
+    flexDirection: "row",
+    height: 50,
+    margin: "2em",
+    marginTop: 0,
+    marginBottom: "1em",
+    justifyContent: "space-between",
+  },
+});
 
 export default Home;
