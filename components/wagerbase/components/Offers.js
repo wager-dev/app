8c27a414a794eef35b917d419f53e7dc8f814a97
Max Ayotte <maxayotte834@gmail.com>
2021-04-18 18:19:56 -0400

    Final push les gooooooooo

--- components/wagerbase/components/Offers.js
+++ components/wagerbase/components/Offers.js
@@ -1,19 +1,20 @@
 import * as React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { FontAwesome } from '@expo/vector-icons'; 
-
+import { WagerText } from './WagerText'
+ 
 import { Colors } from "../util";
 
 export const Offers = ({ requests }) => {
   return (
     <View style={styles.offersBox}>
-      <View style={styles.offersBellIcon}>
-        <FontAwesome name="bell-o" size={24} color="black" />
-      </View>
       <View style={styles.offersContent}>
-        <Text style={styles.offersRequestLine}>{requests} New Wager Requests!</Text>
-        <Text style={styles.offersMessage}> See your current wager requests from your friends </Text>
-        <Text style={styles.offersOpenTag}>OPEN INBOX</Text>
+        <WagerText type="bold"><Text style={styles.offersRequestLine}>{requests} New Wager Requests!</Text></WagerText>
+        <WagerText type="regular"><Text style={styles.offersMessage}>See your current wager requests from your friends </Text></WagerText>
+        <View style={ styles.offersOpenWrapper }><WagerText type="bold"><Text style={styles.offersOpenTag}>OPEN INBOX</Text></WagerText></View>
+      </View>
+      <View style={styles.offersBellIcon}>
+        <FontAwesome name="bell-o" size={24} color="white" />
       </View>
     </View>
   );
@@ -35,25 +36,20 @@ const styles = StyleSheet.create({
   },
   
   offersContent: {
-    // order: 1,
     width: "80%",
-    paddingTop: 20,
-    paddingBottom: 20,
+    paddingTop: 10,
     paddingLeft: 10,
     paddingRight: 20
   },
   
   offersBellIcon: {
-    // order: 2,
     width: "20%",
+    alignItems: "flex-end",
     paddingTop: 10,
-    paddingBottom: 0,
-    paddingLeft: 5,
-    paddingRight: 50
+    paddingRight: 10
   },
   
   offersRequestLine: {
-    fontWeight: "900",
     flexDirection: "row"
   },
   
@@ -63,7 +59,9 @@ const styles = StyleSheet.create({
   },
   
   offersOpenTag: {
-    fontWeight: "900",
     fontSize: 13
+  },
+  offersOpenWrapper: {
+    paddingTop: 20
   }
 })
