89cf491663691f062fb4692d165fc702a6f89cf3
davidmichaelhogan <davidmichaelandco@gmail.com>
2021-05-12 19:04:52 -0400

    updated notes

--- components/Chats.js
+++ components/Chats.js
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { View, ScrollView, StyleSheet } from "react-native";
-import Header from "./Header";
+import { View, ScrollView, StyleSheet, Image, Text } from "react-native";
 
 import { WagerText } from "./wagerbase";
 
@@ -13,11 +12,30 @@ function Chats({ navigation }) {
         height: "100%",
       }}
     >
-      <Header />
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri:
+              "https://pbs.twimg.com/profile_images/1356284638759710720/ELE4hRRQ_x96.jpg",
+          }}
+          style={{
+            width: 50,
+            height: 50,
+            borderBottomRightRadius: 50,
+            borderBottomLeftRadius: 50,
+            borderTopLeftRadius: 50,
+            borderTopRightRadius: 50,
+          }}
+        />
+        <Text>
+          Chat
+        </Text>
+        <Text>
+          Chat
+        </Text>
+      </View>
       <ScrollView>
-        <View style={styles.container}>
-          
-        </View>
+        <View style={styles.container}></View>
       </ScrollView>
     </View>
   );
@@ -37,6 +55,17 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
   },
+  header: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "red",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+
+  },
+  headerOptions: {},
 });
 
 export default Chats;
