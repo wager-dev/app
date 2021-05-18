235f10b9c436cfb9ccc9e2162bbe9eb1867390ff
Max Ayotte <maxayotte834@gmail.com>
2021-05-18 11:59:31 -0400

    More commits to finish the second todo item

---
+++ components/wagerbase/components/TeamLogo.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import { BOS } from "../../NBALogos"
+
+export const TeamLogo = () => {
+  return (
+    <View>
+      <BOS size="80"/>
+    </View>
+  )
+}
